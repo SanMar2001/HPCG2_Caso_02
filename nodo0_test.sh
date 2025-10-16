abf69d61c5eca7090090f7116beb6a9f803874be
SanMar2001 <s.martinez4@utp.edu.co>
2025-10-16 04:04:28 +0000

    Tests implemented and profiling done

---
+++ nodo0_test.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+> nodo0/Secuential.txt
+> nodo0/OMP.txt
+> nodo0/OMP_T.txt
+
+
+for i in 400 800 1000 1200 1600
+do
+    ./sequential $i >> nodo0/Secuential.txt
+done
+
+for i in 400 800 1000 1200 1600
+do
+    for k in 2 4 8
+    do
+        OMP_NUM_THREADS=$k ./OMP $i >> "nodo0/OMP.txt"
+        OMP_NUM_THREADS=$k ./OMP_T $i >> "nodo0/OMP_T.txt"
+    done
+done
---
+++ nodo1_test.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+> nodo1/Secuential.txt
+> nodo1/OMP.txt
+> nodo1/OMP_T.txt
+
+
+for i in 400 800 1000 1200 1600
+do
+    ./sequential $i >> nodo1/Secuential.txt
+done
+
+for i in 400 800 1000 1200 1600
+do
+    for k in 2 4 8
+    do
+        OMP_NUM_THREADS=$k ./OMP $i >> "nodo1/OMP.txt"
+        OMP_NUM_THREADS=$k ./OMP_T $i >> "nodo1/OMP_T.txt"
+    done
+done
